54e01a91d9aa0ccf44a41cae3f0a7e0406ec2f35
Danil <gabitov1983@gmail.com>
2021-05-02 20:33:52 +0300

    add messages memory

--- main.py
+++ main.py
@@ -40,22 +40,21 @@ class MyClient(discord.Client):
                 await message.channel.send('pong')
             elif message.content in ('!tr', '!тр', '!nh', '!ек'):
                 response = await translate(str(message.author))
-                await message.channel.send(f'{str(message.author)}:{response}')
+                await message.channel.send(f'{str(message.author).split("#")[0]}: {response}')
             else:
                 # print(message.author, message.content)
                 await load_to_db(str(message.author), str(message.content))
 
 
 async def load_to_db(author, content):
-    # if Messages.exists(author) == 1:
-    #     print('тут надо добавить сообщение в список сообщений этого автора')
-    # else:
-    #     print('тут надо зарегать новый ключ автора и добавить туда сообщение')
-    Messages.set(author, content, ex=30)
+    if Messages.llen(author) > 5:
+        Messages.lpop(author)
+    Messages.rpush(author, content)
+    Messages.expire(author, 60)
 
 
 async def translate(author):
-    message = Messages.get(author)
+    message = Messages.rpop(author)
     if message:
         message = message.decode('utf-8')
         return message.translate(layout)
